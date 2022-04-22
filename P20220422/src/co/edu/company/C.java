687a2e9683c4655e728d52d3299a5827c48eefa0
admin <admin@DESKTOP-C1PD23B>
2022-04-22 15:29:10 +0900

    2022-04-22-금

--- P20220422/src/co/edu/company/C.java
+++ P20220422/src/co/edu/company/C.java
@@ -12,7 +12,7 @@ public class C extends A { //자식 클래스가 부모 클래스 상속받을 �
 		temp = a.b; //b밖에 안나옴. 퍼블릭이라서
 		
 		//protected
-		C c = new C();ㄴ
+		C c = new C();
 		int d = c.c; //A클래스에 protected로 지정된 c를 extend로 상속받아서 사용 가능 (자식 클래스)
 
 	}
